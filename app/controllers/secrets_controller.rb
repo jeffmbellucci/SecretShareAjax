479ea27f0a083b10cb07588402053151b0689bb3
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-23 17:49:36 -0700

    fix minor form problems

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,16 +2,16 @@ class SecretsController < ApplicationController
   before_filter :require_current_user!
 
   def create
-    params[:secret][:sender_id] = current_user.id
+    params[:secret][:author_id] = current_user.id
     @secret = Secret.create!(params[:secret])
 
     respond_to do |format|
-      format.html { redirect_to :index }
+      format.html { redirect_to user_url(@secret.recipient_id) }
       format.json { render :json => @secret }
     end
   end
 
   def new
-    @recipient_id = params[:id]
+    @recipient_id = params[:user_id]
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :require_current_user!, :except => [:create, :new]
-  before_fitler :require_no_current_user!, :on => [:create, :new]
+  before_filter :require_no_current_user!, :only => [:create, :new]
 
   def create
     @user = User.new(params[:user])
